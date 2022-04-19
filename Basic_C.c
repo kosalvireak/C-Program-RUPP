34f34c1776ea2b707e569fb6f63f32f5f1c852a0
khoeunkosalvireak <virakvary@gmail.com>
2022-04-19 21:48:24 +0700

    Add ID

--- Basic_C.c
+++ Basic_C.c
@@ -4,18 +4,9 @@
  float score;
  char sex;
 int main(){
-    printf("Enter your ID: ");
-    scanf("%i ",&ID );
+    printf("Enter ID:");
+    scanf("%d",&ID);
+    printf("My ID is: %d",ID);
 
-    printf("Enter your GPA: ");
-    scanf("%lf ",&GPA );
-    
-    printf("Enter your score: ");
-    scanf("%f ",&score );
-    
-    printf("Enter your sex: ");
-    scanf("%c ",&sex );
-
-    printf("\n ID = %i \n GPA = %lf \n score = %f \n sex = %c",ID,GPA,score,sex);
     return 0;
 }
