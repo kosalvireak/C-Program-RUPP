42c8ee5848bb52e4c7774abcb0d56214085f7044
khoeunkosalvireak <virakvary@gmail.com>
2022-04-19 21:55:57 +0700

    Add newline

--- Basic_C.c
+++ Basic_C.c
@@ -6,20 +6,18 @@
 int main(){
     printf("Enter ID:");
     scanf("%d",&ID);
-    printf("My ID is: %d",ID);
+    printf("My ID is: %d\n",ID);
 
     printf("Enter GPA:");
     scanf("%lf",&GPA);
-    printf("My GPA is: %lf",GPA);
+    printf("My GPA is: %lf\n",GPA);
 
     printf("Enter score:");
     scanf("%f",&score);
-    printf("My score is: %f",score);
+    printf("My score is: %f\n",score);
 
     printf("Enter you sex:");
     scanf("%c",&sex);
-    printf("My sex is: %c ",sex);
-
-
+    printf("My sex is: %c \n",sex);
     return 0;
 }
