a65be8812d99df070efabf2d8e2a346ffd2ec207
Kosalvireak Khoeun <virakvary@gmail.com>
2022-06-26 23:50:06 +0700

    commit

--- Function.c
+++ Function.c
@@ -2,13 +2,22 @@
 #include <math.h>
 float sq;
 int num,new_num;
+char student1[5],name_fu;
+
 int main(void){
     printf("Enter the number: ");
     scanf("%d",&num);
     new_num = squ(num);
     printf("The number is %d",new_num);
+    name_fu = name();
+    printf("%c",name());
 }
 int squ(n){
     sq = pow(n,2);
     return sq;
+}
+char name(){
+    printf("Enter name:");
+    scanf("%s",&student1);
+    printf("%s",student1);
 }
